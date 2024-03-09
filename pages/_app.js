8b13748efb05bd773468816da89d352444b5fb50
Emjayi <mjs32841@gmail.com>
2024-03-09 22:28:29 +0330

    last one

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,6 @@ import "./input.css"
 import { AnimatePresence, motion } from 'framer-motion'
 import localFont from '@next/font/local'
 
-const euro = localFont({
-    src: './fonts/eurostile.ttf',
-    variable: '--font-euro',
-})
 export default function App({ Component, pageProps, router }) {
     return (
         <div>
