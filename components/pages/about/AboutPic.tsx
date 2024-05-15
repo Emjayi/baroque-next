38fc48c419fa5e1522b98faa036b398ff512b824
Emjayi <mjs32841@gmail.com>
2024-05-15 20:42:15 +0330

    loading

--- components/pages/about/AboutPic.tsx
+++ components/pages/about/AboutPic.tsx
@@ -7,18 +7,18 @@ const AboutPic = ({ src, priority }: { src: StaticImport, priority: boolean }) =
 
     // Image loading effect
     const shimmer = (w: number, h: number) => `
-     <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-       <defs>
-         <linearGradient id="g">
-           <stop stop-color="#00000030" offset="20%" />
-           <stop stop-color="#44444430" offset="50%" />
-           <stop stop-color="#00000030" offset="70%" />
-         </linearGradient>
-       </defs>
-       <rect width="${w}" height="${h}" fill="#00000020" />
-       <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-       <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-     </svg>`;
+    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#00000000" offset="20%" />
+        <stop stop-color="#44444430" offset="50%" />
+        <stop stop-color="#00000000" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#00000020" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
     const toBase64 = (str: string) =>
         typeof window === "undefined"
             ? Buffer.from(str).toString("base64")
