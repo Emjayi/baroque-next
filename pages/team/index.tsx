ab7ab0e228e41d2897e7e5316e07e8f9ab99f101
Emjayi <mjs32841@gmail.com>
2024-06-19 09:59:43 +0330

    wtf

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -62,8 +62,8 @@ const Team = () => {
         <PageWrapper pageName='Team'>
             <TeamImage />
             <div className='flex'>
-                {team.map(({ firstName, lastName, pos, img, year }, index) => (
-                    <Member firstName={firstName} lastName={lastName} key={index + 1} img={img} year={year} pos={pos} />
+                {team.map(({ firstName, lastName, pos, img }, index) => (
+                    <Member firstName={firstName} lastName={lastName} key={index + 1} img={img} pos={pos} />
                 ))}
             </div>
         </PageWrapper>
